f0ade92dfef1aa8510f9e76a45e06fab7905503e
Gerardo Keys <jotojry@icloud.com>
2019-01-21 21:22:01 -0800

    01/21 work update

--- numbers.js
+++ numbers.js
@@ -1,5 +1,5 @@
-/* equation generator FCC */
-
+/*Math practice module FCC */
+alert('Let\'s do some math!');
 operand1 = prompt('Please provide a number: ');
 numOne = parseFloat(operand1);
 operand2 = prompt('Please provide a number: ');
@@ -7,11 +7,13 @@ numTwo = parseFloat(operand2);
 add = numOne + numTwo;
 multiply = numOne * numTwo;
 divide = numOne / numTwo;
-alert('Press return for results.');
+subtract = numOne - numTwo;
 addSolution = "<h2>" + numOne + " + " + numTwo + " = " + add + "</h2>";
 multSolution = "<h2>" + numOne + " * " + numTwo + " = " + multiply + "</h2>";
 divSolution = "<h2>" + numOne + " / " + numTwo + " = " + divide + "</h2>";
-document.write('<h1>Let\'s do some math!</h1><br>');
+subSolution = "<h2>" + numOne + " - " + numTwo + " = " + subtract + "</h2>";
+document.write('<h1>Let\'s do some math with numbers ' + numOne + ' and ' +  numTwo + '!</h1><br>');
 document.write(addSolution + "<br>");
 document.write(multSolution + "<br>");
 document.write(divSolution + "<br>");
+document.write(subSolution + "<br>");
