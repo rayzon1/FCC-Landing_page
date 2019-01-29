8498dc87db68ab6461b935ffe96414785d2808b2
Gerardo Keys <jotojry@icloud.com>
2019-01-28 17:58:21 -0800

    01/28/19 update

--- numbers.js
+++ numbers.js
@@ -1,19 +1,33 @@
-/*Math practice module FCC */
-alert('Let\'s do some math!');
-operand1 = prompt('Please provide a number: ');
-numOne = parseFloat(operand1);
-operand2 = prompt('Please provide a number: ');
-numTwo = parseFloat(operand2);
-add = numOne + numTwo;
-multiply = numOne * numTwo;
-divide = numOne / numTwo;
-subtract = numOne - numTwo;
-addSolution = "<h2>" + numOne + " + " + numTwo + " = " + add + "</h2>";
-multSolution = "<h2>" + numOne + " * " + numTwo + " = " + multiply + "</h2>";
-divSolution = "<h2>" + numOne + " / " + numTwo + " = " + divide + "</h2>";
-subSolution = "<h2>" + numOne + " - " + numTwo + " = " + subtract + "</h2>";
-document.write('<h1>Let\'s do some math with numbers ' + numOne + ' and ' +  numTwo + '!</h1><br>');
-document.write(addSolution + "<br>");
-document.write(multSolution + "<br>");
-document.write(divSolution + "<br>");
-document.write(subSolution + "<br>");
+// declare program variables
+var num1;
+var num2;
+
+// announce the program
+alert("Let's do some math!");
+
+// collect numeric input
+num1 = prompt("Please type a number");
+num1 = parseFloat(num1);
+num2 = prompt("Please type another number");
+if (parseInt(num2) === 0) {
+  alert('Sorry, you cannot divide by ' + num2 + ', reload and try again.');
+  break;
+}
+num2 = parseFloat(num2);
+if (isNaN(num1) || isNaN(num2)) {
+  alert('Sorry, one or more inputs is not a number. Reload and try again.');
+  break;
+}
+// build an HTML message
+var message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
+message += num1 + " + " + num2 + " = " + (num1 + num2);
+message += "<br>";
+message += num1 + " * " + num2 + " = " + (num1 * num2);
+message += "<br>";
+message += num1 + " / " + num2 + " = " + (num1 / num2);
+message += "<br>";
+message += num1 + " - " + num2 + " = " + (num1 - num2);
+  
+// write message to web page
+document.write(message);
+
